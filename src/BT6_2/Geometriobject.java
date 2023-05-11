94d78260c2ccfa35e0c5c3fa4d56d49e6083b79d
dongtun <132561772+dongtun@users.noreply.github.com>
2023-05-11 09:41:13 +0700

    Add files via upload

---
+++ src/BT6_2/Geometriobject.java
@@ -0,0 +1,7 @@
+package BTVN.BT6_2;
+
+public interface Geometriobject {
+    public double getArea();
+    public double getPerimeter();
+
+}
---
+++ src/BT6_2/Rectangle.java
@@ -0,0 +1,29 @@
+package BTVN.BT6_2;
+
+public class Rectangle implements Geometriobject {
+    private double width;
+    private double length;
+
+    public Rectangle(double width, double length) {
+        this.width = width;
+        this.length = length;
+    }
+
+    @Override
+    public double getArea() {
+        return width * length;
+    }
+
+    @Override
+    public double getPerimeter() {
+        return (width + length)* 2;
+    }
+
+    @Override
+    public String toString() {
+        return "Rectangle{" +
+                "width=" + width +
+                ", length=" + length +
+                '}';
+    }
+}
