b6109abb3cbe579c5fe9ac63fddee5748096ab56
dongtun <132561772+dongtun@users.noreply.github.com>
2023-05-11 09:42:02 +0700

    Add files via upload

---
+++ src/BT6_3/Movable.java
@@ -0,0 +1,8 @@
+package BTVN.BT6_3;
+
+ public abstract class Movable {
+    abstract void moveUp();
+    abstract void moveDown();
+    abstract void moveLeft();
+    abstract void moveRight();
+}
---
+++ src/BT6_3/MovablePonit.java
@@ -0,0 +1,40 @@
+package BTVN.BT6_3;
+
+class MovablePoint extends Movable {
+    private int x;
+    private int y;
+    private int xSpeed;
+    private int ySpeed;
+
+    public MovablePoint(int x, int y, int xSpeed, int ySpeed) {
+        this.x = x;
+        this.y = y;
+        this.xSpeed = xSpeed;
+        this.ySpeed = ySpeed;
+    }
+
+    @Override
+    public void moveUp() {
+        y -= ySpeed;
+    }
+
+    @Override
+    public void moveDown() {
+        y += ySpeed;
+    }
+
+    @Override
+    public void moveLeft() {
+        x -= xSpeed;
+    }
+
+    @Override
+    public void moveRight() {
+        x += xSpeed;
+    }
+
+    @Override
+    public String toString() {
+        return "MovablePoint: (" + x + ", " + y + ")";
+    }
+}
